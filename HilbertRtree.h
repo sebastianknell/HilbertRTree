0ff1943d7850ee393a992767d2900bd1193c92fb
Sebastian Knell <sebastianknell@hotmail.com>
2022-06-14 10:53:26 -0500

    definitions

--- HilbertRtree.h
+++ HilbertRtree.h
@@ -5,9 +5,30 @@
 #ifndef HILBERT_RTREE_HILBERTRTREE_H
 #define HILBERT_RTREE_HILBERTRTREE_H
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+using Point = struct {int x, y;};
+using Rect = struct {int xlow, xhigh, ylow, yhigh;};
+using Data = vector<Point>;
+
+struct Node {
+    bool isLeaf;
+    Node* prev;
+    int lhv;
+    vector<Rect> regions;
+    vector<Node*> childs;
+    vector<Data*> data;
+};
 
 class HilbertRtree {
-
+    Node* root;
+    static int getHilbertIndex();
+public:
+    void insert(Data);
+    void remove(Data);
 };
 
 
